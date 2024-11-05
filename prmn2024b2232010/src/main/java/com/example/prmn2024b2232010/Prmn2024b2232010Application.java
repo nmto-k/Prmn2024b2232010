ce1a303496873aea0106d2827a34f51c0a04e82c
nmto-k <b2232010@photon.chitose.ac.jp>
2024-11-05 14:40:00 +0900

    プロジェクト作成

---
+++ prmn2024b2232010/src/main/java/com/example/prmn2024b2232010/Prmn2024b2232010Application.java
@@ -0,0 +1,13 @@
+package com.example.prmn2024b2232010;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prmn2024b2232010Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prmn2024b2232010Application.class, args);
+    }
+
+}
---
+++ prmn2024b2232010/src/test/java/com/example/prmn2024b2232010/Prmn2024b2232010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prmn2024b2232010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prmn2024b2232010ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
